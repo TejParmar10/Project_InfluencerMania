f768de5f29ee1ebc371ac75b5e7b63a2243d2244
shubh <darjishubh78@gmail.com>
2022-05-21 09:21:39 +0530

    commit 6

--- html/index.php
+++ html/index.php
@@ -5,13 +5,13 @@ include 'header.php';
       <div class="page-banner home-banner">
         <div class="row align-items-center flex-wrap-reverse h-100">
           <div class="col-md-6 py-5 wow fadeInLeft">
-            <h1 class="mb-4">Let's Check and Optimize your website!</h1>
-            <p class="text-lg text-grey mb-5">Ignite the most powerfull growth engine you have ever built for your company</p>
-            <a href="#" class="btn btn-primary btn-split">Watch Video <div class="fab"><span class="mai-play"></span></div></a>
+            <h1 class="mb-4">Let's Connect!</h1>
+            <p class="text-lg text-grey mb-5">We Connect Innovative Brands with Influencers & Content Creators around the world..</p>
+            <!-- <a href="#" class="btn btn-primary btn-split">Watch Video <div class="fab"><span class="mai-play"></span></div></a> -->
           </div>
           <div class="col-md-6 py-5 wow zoomIn">
             <div class="img-fluid text-center">
-              <img src="../assets/img/banner_image_1.svg" alt="">
+              <img src="../assets/img/slider-icon.png" alt="" width="450px">
             </div>
           </div>
         </div>
